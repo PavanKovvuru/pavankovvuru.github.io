a2f016452779bbea76a080ed20722b1fd9dee79e
Pavan Kovvuru <36547326+PavanKovvuru@users.noreply.github.com>
2020-01-29 15:31:59 -0700

    Update game.js

--- game.js
+++ game.js
@@ -15,7 +15,7 @@ function create() {
   
   const platforms = this.physics.add.staticGroup();
 
-  platforms.create(225, 550, 'platform');
+  platforms.create(225, 510, 'platform');
 
   gameState.player.setCollideWorldBounds(true);
 
