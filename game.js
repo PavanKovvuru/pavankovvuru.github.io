3dd2fb581264445a488eab9016aa8c53f8bddb9c
Pavan Kovvuru <36547326+PavanKovvuru@users.noreply.github.com>
2020-01-29 18:10:12 -0700

    Update game.js

--- game.js
+++ game.js
@@ -6,10 +6,10 @@ function preload() {
   this.load.image('bug1', 'https://s3.amazonaws.com/codecademy-content/courses/learn-phaser/physics/bug_1.png')
   this.load.image('bug2', 'https://s3.amazonaws.com/codecademy-content/courses/learn-phaser/physics/bug_2.png')
   this.load.image('bug3', 'https://s3.amazonaws.com/codecademy-content/courses/learn-phaser/physics/bug_3.png')
-  this.load.image('platform', 'https://s3.amazonaws.com/codecademy-content/courses/learn-phaser/physics/platform.png')
+  this.load.image('platform', 'https://raw.githubusercontent.com/PavanKovvuru/pavankovvuru.github.io/master/New%20Piskel-1.png%20(4).png')
   this.load.image('codey', 'https://raw.githubusercontent.com/PavanKovvuru/pavankovvuru.github.io/master/New%20Piskel-1.png%20(3).png')
 }
-
+// https://s3.amazonaws.com/codecademy-content/courses/learn-phaser/physics/platform.png
 function create() {
   gameState.player = this.physics.add.sprite(225, 450, 'codey').setScale(.5);
   
