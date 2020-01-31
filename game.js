817355c8947cc4911631407bcb580e6a2c1cc934
Pavan Kovvuru <36547326+PavanKovvuru@users.noreply.github.com>
2020-01-31 10:28:13 -0700

    Update game.js

--- game.js
+++ game.js
@@ -70,7 +70,7 @@ function create() {
     this.physics.add.collider(gameState.player, bugs, () => {
     bugGenLoop.destroy();
     this.physics.pause();
-    this.add.text(700, 250, 'Game Over', { fontSize: '80px', fill: '#ffffff' });
+    this.add.text(700, 220, 'Game Over', { fontSize: '80px', fill: '#ffffff' });
     this.add.text(680, 350, 'Click to Restart', { fontSize: '50px', fill: '#ffffff' });
     
 		// Add your code below:
