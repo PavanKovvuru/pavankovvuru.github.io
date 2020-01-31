3f694909dd6ed41db6e501ff1e1d602e419fc7c3
Pavan Kovvuru <36547326+PavanKovvuru@users.noreply.github.com>
2020-01-30 19:51:43 -0700

    Update game.js

--- game.js
+++ game.js
@@ -59,8 +59,8 @@ function create() {
   
   // Add your code below:
   this.physics.add.collider(bugs, platforms);
-  this.physics.add.collider(bugs, walls, funtion (bug) {
-   bug.destroy();
+  this.physics.add.collider(bugs, walls, funtion (bugs) {
+   bugs.destroy();
   //  gameState.score += 10;
   //  gameState.scoreText.setText(`Score: ${gameState.score}`);
   })
