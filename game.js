f6ebe7bf006e9303e4fd6eb2266fbb890139b16f
Pavan Kovvuru <36547326+PavanKovvuru@users.noreply.github.com>
2020-01-30 22:49:43 -0700

    Update game.js

--- game.js
+++ game.js
@@ -93,7 +93,7 @@ function update() {
     gameState.player.setVelocityX(200);
     //gameState.bugs.setVelocityX(20);
   } else {
-    gameState.player.setVelocityX(0);
+    gameState.player.setVelocityX(-1);
   }
     if ((gameState.cursors.space.isDown || 
      gameState.cursors.up.isDown)&& gameState.player.body.touching.down) {
