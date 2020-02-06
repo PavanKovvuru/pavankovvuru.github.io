46858cc6aa27564e500bbf7f109786c90434274b
Pavan Kovvuru <36547326+PavanKovvuru@users.noreply.github.com>
2020-02-06 10:19:42 -0700

    Update game1.js

--- game1.js
+++ game1.js
@@ -20,7 +20,7 @@ function create() {
   const walls = this.physics.add.staticGroup();
   const xCoord = Math.random() * 1900;
    platforms.create(225, 680, 'platform').setScale(1, .3).refreshBody();
-   platforms.create(xCoord, 680, 'platform').setScale(.3, 1).refreshBody();
+   platforms.create(xCoord, 680, 'platform').setScale(.1, .3).refreshBody();
   // platforms.create(800, 680, 'platform').setScale(1, .3).refreshBody();
   // platforms.create(1100, 680, 'platform').setScale(1, .3).refreshBody();
   // platforms.create(1500, 680, 'platform').setScale(1, .3).refreshBody();
