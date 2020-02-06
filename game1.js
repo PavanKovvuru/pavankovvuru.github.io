b5171a210ab66558123de08c8af5fdd2b5b6bdc7
Pavan Kovvuru <36547326+PavanKovvuru@users.noreply.github.com>
2020-02-06 10:41:48 -0700

    Update game1.js

--- game1.js
+++ game1.js
@@ -106,7 +106,7 @@ this.physics.add.collider(gameState.player, bugs, () => {
 this.physics.add.collider(gameState.player, walls, () => {
    bugGenLoop.destroy();
     this.physics.pause();
-    this.add.text(700, 220, 'Game Lost', { fontSize: '80px', fill: '#ffffff' });
+    this.add.text(700, 220, 'Game Won', { fontSize: '80px', fill: '#ffffff' });
     this.add.text(680, 350, 'Click to Restart', { fontSize: '50px', fill: '#ffffff' });
     
 		// Add your code below:
