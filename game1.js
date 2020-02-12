6ab46f11bc7b2ab22544d03653873100c33a1bfe
Pavan Kovvuru <36547326+PavanKovvuru@users.noreply.github.com>
2020-02-12 11:27:00 -0700

    Update game1.js

--- game1.js
+++ game1.js
@@ -180,7 +180,7 @@ function update() {
   }
     if ((gameState.cursors.space.isDown || 
      gameState.cursors.up.isDown)&& gameState.player.body.touching.down) {
-     gameState.player.setVelocityY(-300);
+     gameState.player.setVelocityY(-100);
   }
  
 }
@@ -198,7 +198,7 @@ const config = {
   physics: {
     default: 'arcade',
     arcade: {
-      gravity: { y: 500 },
+      gravity: { y: 100 },
       enableBody: true,
     }
   },
