569c322e3dcb2dec2dd7997cc3cbec69e5355c0b
Pavan Kovvuru <36547326+PavanKovvuru@users.noreply.github.com>
2020-02-18 14:54:22 -0700

    Update game1.js

--- game1.js
+++ game1.js
@@ -98,7 +98,7 @@ function create() {
    platforms.create(1650, bCoord+5, 'platform').setScale(.05, .3).refreshBody();
    platforms.create(1950, abCoord+605, 'platform').setScale(1, .3).refreshBody();
   // walls.create(1850, 595, 'wall').setScale(.3, 1).refreshBody();
-  gameState.player = this.physics.add.sprite(200, mCoord-40, 'codey').setScale(.5);
+  gameState.player = this.physics.add.sprite(200, mCoord+40, 'codey').setScale(.5);
 //  gameState.player.setCollideWorldBounds(true);
    this.physics.add.collider(gameState.player, platforms);
   // this.physics.add.collider(gameState.player, walls);
