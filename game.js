9a9f553767af7749865ca2bc7a1412884d1bc3e4
Pavan Kovvuru <36547326+PavanKovvuru@users.noreply.github.com>
2020-02-20 12:13:14 -0700

    Update game.js

--- game.js
+++ game.js
@@ -30,7 +30,7 @@ function create() {
    platforms.create(1500, 680, 'platform').setScale(1, .3).refreshBody();
    platforms.create(1800, 680, 'platform').setScale(1, .3).refreshBody();
    platforms.create(1900, 680, 'platform').setScale(1, .3).refreshBody();
-   walls.create(1890, 595, 'wall').setScale(.3, 1).refreshBody();
+   walls.create(1892, 595, 'wall').setScale(.3, 1).refreshBody();
    walles.create(5, 595, 'walle').setScale(.3, 1).refreshBody();
   gameState.player.setCollideWorldBounds(true);
 
