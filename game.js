5110443e8e76ea5e1b368fdeb9554e0bdc6d5ac0
Pavan Kovvuru <36547326+PavanKovvuru@users.noreply.github.com>
2020-02-20 11:26:02 -0700

    Update game.js

--- game.js
+++ game.js
@@ -6,6 +6,7 @@ function preload() {
   this.load.image('bug1', 'https://raw.githubusercontent.com/PavanKovvuru/pavankovvuru.github.io/master/New%20Piskel-1.png%20(6).png')
   this.load.image('platform', 'https://raw.githubusercontent.com/PavanKovvuru/pavankovvuru.github.io/master/New%20Piskel-1.png%20(4).png')
   this.load.image('wall', 'https://raw.githubusercontent.com/PavanKovvuru/pavankovvuru.github.io/master/New%20Piskel-1.png%20(7).png')
+  this.load.image('walle', 'https://raw.githubusercontent.com/PavanKovvuru/pavankovvuru.github.io/master/New%20Piskel-1.png%20(7).png')
   this.load.image('codey', 'https://raw.githubusercontent.com/PavanKovvuru/pavankovvuru.github.io/master/New%20Piskel-1.png.png')
   this.load.image('snow', 'https://raw.githubusercontent.com/PavanKovvuru/pavankovvuru.github.io/master/snowimage.png')
 }
@@ -20,6 +21,7 @@ function create() {
   
   const platforms = this.physics.add.staticGroup();
   const walls = this.physics.add.staticGroup();
+  const walles = this.physics.add.staticGroup();
 
    platforms.create(225, 680, 'platform').setScale(1, .3).refreshBody();
    platforms.create(400, 680, 'platform').setScale(1, .3).refreshBody();
@@ -29,6 +31,7 @@ function create() {
    platforms.create(1800, 680, 'platform').setScale(1, .3).refreshBody();
    platforms.create(1900, 680, 'platform').setScale(1, .3).refreshBody();
    walls.create(1850, 595, 'wall').setScale(.3, 1).refreshBody();
+   walles.create(30, 595, 'walle').setScale(.3, 1).refreshBody();
   gameState.player.setCollideWorldBounds(true);
 
    this.physics.add.collider(gameState.player, platforms);
@@ -103,12 +106,25 @@ function create() {
      this.scene.restart();
     });
   });
+    this.physics.add.collider(gameState.player, walles, () => {
+    bugGenLoop.destroy();
+    snowGenLoop.destroy();
+    this.physics.pause();
+    this.add.text(700, 220, 'Game Over', { fontSize: '80px', fill: '#ffffff' });
+    this.add.text(680, 350, 'Click to Restart', { fontSize: '50px', fill: '#ffffff' });
+    
+		// Add your code below:
+     this.input.on('pointerup', () =>{
+    gameState.score = 0;
+     this.scene.restart();
+    });
+  });
 
-this.cursors = this.input.keyboard.addKeys(
-{up:Phaser.Input.Keyboard.KeyCodes.W,
-down:Phaser.Input.Keyboard.KeyCodes.S,
-left:Phaser.Input.Keyboard.KeyCodes.A,
-right:Phaser.Input.Keyboard.KeyCodes.D});
+//this.cursors = this.input.keyboard.addKeys(
+//{up:Phaser.Input.Keyboard.KeyCodes.W,
+//down:Phaser.Input.Keyboard.KeyCodes.S,
+//left:Phaser.Input.Keyboard.KeyCodes.A,
+//right:Phaser.Input.Keyboard.KeyCodes.D});
 }
 function update() {
    //player.body.setZeroVelocity();
