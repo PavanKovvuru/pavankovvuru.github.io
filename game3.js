c9c3f2a18ca769300c07706753e400c4a5790b04
Pavan Kovvuru <36547326+PavanKovvuru@users.noreply.github.com>
2020-02-21 10:59:51 -0700

    Update game3.js

--- game3.js
+++ game3.js
@@ -1,5 +1,5 @@
-
-var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create, update: update, render: render });
+const gameState = {};
+//var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create, update: update, render: render });
 
 function preload() {
 
@@ -11,26 +11,26 @@ var flyer;
 
 function create() {
 
-    game.physics.startSystem(Phaser.Physics.ARCADE);
+    const game.physics.startSystem(Phaser.Physics.ARCADE);
 
-    cursors = game.input.keyboard.createCursorKeys();
+    const cursors = game.input.keyboard.createCursorKeys();
 
-    flyer = game.add.sprite(400, 200, 'play');
+    const flyer = game.add.sprite(400, 200, 'play');
 
    // flyer.animations.add('left', [0, 1, 2, 3], 10, true);
    // flyer.animations.add('right', [5, 6, 7, 8], 10, true);
 
-    game.physics.enable(flyer, Phaser.Physics.ARCADE);
+   const  game.physics.enable(flyer, Phaser.Physics.ARCADE);
     
     //  This gets it moving
-    flyer.body.velocity.setTo(200, 200);
+   const  flyer.body.velocity.setTo(200, 200);
     
     //  This makes the game world bounce-able
-    flyer.body.collideWorldBounds = true;
+    const flyer.body.collideWorldBounds = true;
     
     //  This sets the image bounce energy for the horizontal 
     //  and vertical vectors (as an x,y point). "1" is 100% energy return
-    flyer.body.bounce.setTo(0.8, 0.8);
+   const  flyer.body.bounce.setTo(0.8, 0.8);
 
 }
 
@@ -84,9 +84,28 @@ function update () {
     
 }
 
-function render () {
+//function render () {
 
     //debug helper
     //game.debug.spriteInfo(flyer,32,32);
 
-}
+//}
+const config = {
+  type: Phaser.AUTO,
+  width: 1900,
+  height: 920,
+  backgroundColor: "#566061",
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 500 },
+      enableBody: true,
+    }
+  },
+  scene: {
+    preload,
+    create,
+    update
+  }
+};
+const game = new Phaser.Game(config);
