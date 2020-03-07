52b3a0b84b051fca49f9082bbdf0b3558644304a
Pavan Kovvuru <36547326+PavanKovvuru@users.noreply.github.com>
2020-03-06 19:07:59 -0700

    Update game.js

--- game.js
+++ game.js
@@ -23,13 +23,8 @@ function create() {
   const walls = this.physics.add.staticGroup();
   const walles = this.physics.add.staticGroup();
 
-   platforms.create(225, 680, 'platform').setScale(1, .3).refreshBody();
-   platforms.create(400, 680, 'platform').setScale(1, .3).refreshBody();
-   platforms.create(800, 680, 'platform').setScale(1, .3).refreshBody();
-   platforms.create(1100, 680, 'platform').setScale(1, .3).refreshBody();
-   platforms.create(1500, 680, 'platform').setScale(1, .3).refreshBody();
-   platforms.create(1800, 680, 'platform').setScale(1, .3).refreshBody();
-   platforms.create(1900, 680, 'platform').setScale(1, .3).refreshBody();
+
+   platforms.create(1100, 680, 'platform').setScale(500, .3).refreshBody();
    walls.create(1894, 595, 'wall').setScale(.3, 1).refreshBody();
    walles.create(5, 595, 'walle').setScale(.3, 1).refreshBody();
   gameState.player.setCollideWorldBounds(true);
