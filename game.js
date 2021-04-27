257b94214d5ab4ce31e9286e48d3f7e72801a071
Pavan Kovvuru <36547326+PavanKovvuru@users.noreply.github.com>
2021-04-27 11:29:37 -0600

    Update game.js

--- game.js
+++ game.js
@@ -93,7 +93,7 @@ function create() {
     bugGenLoop.destroy();
     snowGenLoop.destroy();
     this.physics.pause();
-    this.add.text(700, 220, 'Game Over', { font: 'bold 80px Arial', fontSize: '80px', fill: '#ffffff' });
+    this.add.text(700, 220, 'Game Over', { fontSize: '80px', fill: '#ffffff' });
     this.add.text(680, 350, 'Click to Restart', { fontSize: '50px', fill: '#ffffff' });
     
 		// Add your code below:
